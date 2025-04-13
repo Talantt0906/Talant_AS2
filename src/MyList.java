2bdd2bfaf3fa0ee5181aa8a79445a06f71fe9c2d
Talantt0906 <talante0906@gmail.com>
2025-04-13 21:19:07 +0500

    MyList interface

---
+++ src/MyList.java
@@ -0,0 +1,25 @@
+/**
+ * MyList is a custom list interface with methods to add, remove,
+ * get elements, sort the list, and convert it to an array.
+ * It extends Iterable<T> for easy iteration.
+ */
+public interface MyList<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    Object[] toArray();
+    void clear();
+    int size();
+}
